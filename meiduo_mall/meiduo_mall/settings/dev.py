46b610210305cc7acc36a6531442f6725c87b624
Hillary886 <zhangxu201709@163.com>
2021-07-07 17:41:17 +0200

    Configure database MySQL

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = 'meiduo_mall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', # 数据库引擎
+        'HOST': '192.168.206.134', # 数据库主机
+        'PORT': 3306, # 数据库端口
+        'USER': 'Hillary886', # 数据库用户名
+        'PASSWORD': 'x56939980', # 数据库用户密码
+        'NAME': 'meiduo' # 数据库名字
     }
 }
 
